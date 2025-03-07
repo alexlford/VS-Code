06c9565d78871c0cfaeb0e34202d04e1f79bb41d
alexlford <alexford35@gmail.com>
2025-03-06 19:04:34 -0700

    .

--- pset4/output.sql
+++ pset4/output.sql
@@ -1,29 +1,41 @@
 
+USE nj_state_teachers_salaries;
+
 SELECT 
-    'id',
-    'first_name',
-    'last_name',
-    'primary_job',
-    'experience_total',
-    'experience_district',
-    'experience_current_job',
-    'salary',
-    'days_worked',
-    'teaching_route',
-    'subcategory'
+    'id' AS id,
+    'last_name' AS last_name,
+    'first_name' AS first_name,
+    'county' AS county,
+    'district' AS district,
+    'school' AS school,
+    'primary_job' AS primary_job,
+    'fte' AS fte,
+    'salary' AS salary,
+    'certificate' AS certificate,
+    'subcategory' AS subcategory,
+    'teaching_route' AS teaching_route,
+    'highly_qualified' AS highly_qualified,
+    'experience_district' AS experience_district,
+    'experience_nj' AS experience_nj,
+    'experience_total' AS experience_total
 UNION ALL
 SELECT
     id,
-    first_name,
-    last_name,
-    primary_job,
-    experience_total,
-    experience_district,
-    experience_current_job,
-    salary,
-    days_worked,
-    teaching_route,
-    subcategory
+last_name,
+first_name,
+county,
+district,
+school,
+primary_job,
+fte,
+salary,
+certificate,
+subcategory,
+teaching_route,
+highly_qualified,
+experience_district,
+experience_nj,
+experience_total
 FROM teachers_salaries_pset4
 ORDER BY RAND(7)
 LIMIT 777
